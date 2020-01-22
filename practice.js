83a8d71ed031bd5817281653ab22dfaaed1afb6d
ammarh <ammarhaq@outlook.com>
2020-01-22 13:46:14 -0800

    So the default way to make ajax requests in jquery is via $.ajax( { url : type get post put and then success : funciton(param) here you will use the param to accomplish something like print out the value you changed. With fetch existing in es6 and axios being a thing, I can't seem to find why it would be used

--- practice.js
+++ practice.js
@@ -25,4 +25,40 @@ $(document).ready(function(){
         console.log("Don't look @ me");
     }
     )
+
+    // If someone clicks the button, the text below will have 
+    $('[name=cl]').bind('click', function() {
+
+        console.log("Api stuff");
+        
+        let a = { userID : 1,  title : "test", body : "epsom"}
+        $.ajax(
+            {
+                url : "https://jsonplaceholder.typicode.com/posts/",
+                type : "POST",
+                data : a,
+                dataType: "JSON",
+                success : function(result)
+                {
+                    // Print out the result
+                    console.log("Before")
+                    console.log(result);
+                }
+            }
+        )
+
+        $.ajax(
+            {
+                url : "https://jsonplaceholder.typicode.com/posts/",
+                type : "GET",
+                dataType: "JSON",
+                success : function(result)
+                {
+                    // Print out the result
+                    console.log(result);
+                }
+            }
+        )
+    })
+    
 });
