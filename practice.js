feb59241438dc48c2b001693561d95e8646180cd
unknown <ammarhaq@outlook.com>
2020-01-21 23:44:52 -0800

    So $ (this) is a jquery object of the current DOm element which is what this refers too via the current scope of the item so in a button event handler that's a button. Some selectors: ('ite) all html elements of ite. ('#ia') all elements of id ia (.id) all elements with class id and ([href*=a]) all elements with substring a in their href attribute (*) all elements and (:empty) for all empty elements ( and so on e.g checked button etc)

--- practice.js
+++ practice.js
@@ -1,14 +1,28 @@
 
 // Want to do stuff once the DOM has loaded.
 $(document).ready(function(){ 
-
-
-
-
     // Want to do stuff once all of the images ( assets have loaded )
     $(window).on("load", function() {
         console.log("all assets have loaded");
     })
 
     console.log("yay!");
+
+
+    $('button').bind( 'click',
+    function()
+    {
+        console.log("I was clicked :(");
+        console.log("Convetional this refers to current DOM element (button)"
+         + this + " Jquery this is object based " + $(this)  );
+    }
+    )
+
+    // So I can bind any events ( both custom created and default) https://www.w3schools.com/jsref/dom_obj_event.asp
+    $('#clickMe').bind('mouseenter',
+    function( )
+    {
+        console.log("Don't look @ me");
+    }
+    )
 });
